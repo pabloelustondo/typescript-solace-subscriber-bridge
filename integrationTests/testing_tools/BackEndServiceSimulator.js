c62847bfb7c76e39335613fa7d30455093207648
pabloelustondo <pablo.elustondo@gmail.com>
2023-05-01 22:22:17 -0400

    externalized test constants from mock-server

--- integrationTests/testing_tools/BackEndServiceSimulator.js
+++ integrationTests/testing_tools/BackEndServiceSimulator.js
@@ -1,26 +1,21 @@
 const http = require('http');
 const fs = require('fs');
 const MongoClient = require('mongodb').MongoClient;
+const getConfig = require('./getConfig');
+const argv = require('yargs').argv;
 
 // MongoDB connection URL
-const url = 'mongodb://localhost:27017';
 
-// Create a new MongoClient
-const client = new MongoClient(url);
+
+
 
 
 const logtimestamp = (new Date()).toLocaleTimeString();
 const logfilename1 = `./logs/qb_consumer-log${logtimestamp}.log`;
 const logfilename = logfilename1.replace(":","-")
 
-const hostname = '127.0.0.1';
-const port = 3000;
 let count = 0;
 let countSuccess = 0;
-
-const ALWAYS_SUCCEEDS = true;
-const ALWAYS_FAILS = false;
-const FAILURE_RATE = 2;  // 2 => 50%   3=>33%   4=>25%...etc
 
 
 function writeToLogs(message) { 
@@ -35,11 +30,16 @@ function writeToLogs(message) {
 
 async function run() {
 
+
+  const config = await getConfig(argv);
+    // Create a new MongoClient
+  const client = new MongoClient(config.server.url);
+
 // Connect to MongoDB
   try {
     await client.connect();
     console.log('SERVER Connected to MongoDB!');
-    await client.db('qb_stats').dropDatabase();
+    await client.db(config.server.test_db_name).dropDatabase();
     console.log('SERVER Database dropped');
   } catch(e) { 
     console.log(`SERVER FAILED Connected to MongoDB! ${e}`);
@@ -48,7 +48,7 @@ async function run() {
 
   const server = http.createServer(async (req, res) => {
     // Send a response back to the client
-    if (ALWAYS_SUCCEEDS || ((!ALWAYS_FAILS) && (count % FAILURE_RATE == 0))) {
+    if (config.server.ALWAYS_SUCCEEDS || ((!config.server.ALWAYS_FAILS) && (count % config.server.FAILURE_RATE == 0))) {
       res.statusCode = 200;
       countSuccess++;
       console.log(`SERVER SEND 200  ok processed ${countSuccess} total processed ${count}`)
@@ -63,7 +63,7 @@ async function run() {
     try {
       collectionName = `req-${parsedUrl.replace("/", "")}-${res.statusCode}`
               // Insert the requested URL and timestamp into the database
-      const result = await client.db('qb_stats').collection(collectionName).insertOne({
+      const result = await client.db(config.server.test_db_name).collection(collectionName).insertOne({
           url: parsedUrl,
           statusCode: res.statusCode,
           timestamp: new Date()
@@ -78,8 +78,8 @@ async function run() {
       }
   });
 
-  server.listen(port, hostname, () => {
-    writeToLogs(`SERVER running at http://${hostname}:${port}/`);
+  server.listen(config.server.port, config.server.hostname, () => {
+    writeToLogs(`SERVER running at http://${config.server.hostname}:${config.server.port}/`);
   });
 
 }
